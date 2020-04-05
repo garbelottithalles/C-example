702fd7d17abbe309e2fd04fe80dc565a668ffebe
garbelottithalles <thallesgarbelotti@gmail.com>
2020-04-05 20:16:23 -0300

    updates

--- calculator.c
+++ calculator.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <math.h>
 
-int main ()
-{
-    float n1, n2;
-    char op;
+float n1, n2;
+char op;
 
-    printf("Type M=MULT, D=DIV, A=ADD, S=SUB, P=POW and R=SQRT:\n");
-    scanf("%c", &op);
+float scan() {
     scanf("%f", &n1);
     scanf("%f", &n2);
-    
+}
+float root() {
+    scanf("%f", &n1);
+    printf("%f", sqrt(n1));
+}
+
+int main ()
+{
+    printf("Type M=MULT, D=DIV, A=ADD, S=SUB, P=POW and R=SQRT:\n");
+    scanf("%c", &op);
     switch (op)
     {
         case 'M' :
+            scan();
             printf("%f", n1*n2);
         break;
 
         case 'D':
+            scan();
             printf("%f", n1/n2);
         break;
 
         case 'A':
+            scan();
             printf("%f", n1+n2);
         break;
 
         case 'S':
+            scan();
             printf("%f", n1-n2);
         break;
 
         case 'P':
+            scan();
             printf("%f", pow(n1, n2));
         break;
 
         case 'R':
-            printf("%f && %f", sqrt(n1), sqrt(n2));
+           root();
         break;
-          
     }
-    return 0;
 }
